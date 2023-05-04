eaebf0921a0600440eec166f538ec327d9deacad
Gitznik <R.Offner@gmx.de>
2023-05-04 20:42:28 +0200

    Implement html composer

--- src/routes/about/get.rs
+++ src/routes/about/get.rs
@@ -1,6 +1,10 @@
 use actix_web::{get, HttpResponse, Responder};
 
+use crate::html_base::compose_html;
+
 #[get("/about")]
 async fn about() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("get.html"))
+    let main_div = include_str!("get.html");
+    let html = compose_html(main_div);
+    HttpResponse::Ok().body(html)
 }
--- src/routes/scores/get.rs
+++ src/routes/scores/get.rs
@@ -1,6 +1,10 @@
 use actix_web::{get, HttpResponse, Responder};
 
+use crate::html_base::compose_html;
+
 #[get("/scores")]
 async fn add_scores() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("get.html"))
+    let main_div = include_str!("get.html");
+    let html = compose_html(main_div);
+    HttpResponse::Ok().body(html)
 }
