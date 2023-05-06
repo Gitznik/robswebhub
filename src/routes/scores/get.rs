b3b603ca05db4f0484450538d137e5bf324f59e9
Gitznik <R.Offner@gmx.de>
2023-05-06 19:38:33 +0200

    Pass not implemented message

--- src/routes/scores/get.rs
+++ src/routes/scores/get.rs
@@ -1,10 +1,19 @@
 use actix_web::{get, HttpResponse, Responder};
+use std::fmt::Write;
+use actix_web_flash_messages::IncomingFlashMessages;
 
 use crate::html_base::compose_html;
 
 #[get("/scores")]
-async fn add_scores() -> impl Responder {
+async fn add_scores(flash_messages: IncomingFlashMessages) -> impl Responder {
+    let mut error_html = String::new();
+    writeln!(error_html, r#"<section class="container">"#).unwrap();
+    for m in flash_messages.iter() {
+        writeln!(error_html, r#"<p><i><mark>{}</mark></i></p>"#, m.content()).unwrap();
+    }
+    writeln!(error_html, r#"</section>"#).unwrap();
     let main_div = include_str!("get.html");
-    let html = compose_html(main_div);
+    let main_div = format!("{}\n{}", &error_html, &main_div);
+    let html = compose_html(&main_div);
     HttpResponse::Ok().body(html)
 }
--- src/routes/scores/post.rs
+++ src/routes/scores/post.rs
@@ -1,6 +1,8 @@
 use actix_web::{post, HttpResponse, Responder, http::header::LOCATION};
+use actix_web_flash_messages::FlashMessage;
 
 #[post("/scores")]
 async fn save_scores() -> impl Responder {
+    FlashMessage::info("Sorry, I have not implemented saving scores yet.").send();
     HttpResponse::SeeOther().insert_header((LOCATION, "/scores")).finish()
 }
