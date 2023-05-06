f6e4abb4d50a0077524f396ef59884ef76272236
Gitznik <R.Offner@gmx.de>
2023-05-06 19:38:27 +0200

    Add flash messages framework

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use actix_web::{App, HttpServer};
+use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
 use robswebhub::{
     configuration::get_configuration,
     routes::{about::get::about, root::get::root, scores::{get::add_scores, post::save_scores}},
 };
 use actix_files::Files;
+use actix_web::cookie::Key;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().unwrap();
-    HttpServer::new(|| {
+    let message_store = CookieMessageStore::builder(Key::generate()).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
+    HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .service(root)
             .service(about)
             .service(add_scores)
