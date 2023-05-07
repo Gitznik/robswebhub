0b7f5f7061ee02d49ee268e101cb4d72043456bc
Gitznik <R.Offner@gmx.de>
2023-05-07 17:44:31 +0200

    Add postgres connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_files::Files;
 use actix_web::cookie::Key;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
+use robswebhub::configuration::DatabaseSettings;
 use robswebhub::{
     configuration::get_configuration,
     routes::{
@@ -10,12 +11,23 @@ use robswebhub::{
         scores::{get::add_scores, post::save_scores},
     },
 };
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+
+async fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.connection_string)
+        .expect("Could not connect to the database")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let config = get_configuration().unwrap();
+    let configuration = get_configuration().unwrap();
     let message_store = CookieMessageStore::builder(Key::generate()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let pg_pool = get_connection_pool(&configuration.database).await;
+    let pg_pool = web::Data::new(pg_pool);
     HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())
@@ -24,8 +36,12 @@ async fn main() -> std::io::Result<()> {
             .service(add_scores)
             .service(save_scores)
             .service(Files::new("/images", "./images"))
+            .app_data(pg_pool.clone())
     })
-    .bind((config.application.host, config.application.port))?
+    .bind((
+        configuration.application.host,
+        configuration.application.port,
+    ))?
     .run()
     .await
 }
